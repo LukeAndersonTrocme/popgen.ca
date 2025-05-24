5e2686007cff02446e44e9618d5ea9543f115e56
Luke Anderson-Trocmé <luke.anderson.trocme@gmail.com>
2025-05-24 12:17:56 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://popgen.ca',
-  output: 'static',        // static build is built‑in in Astro 4
+  output: 'static',        // static build is built-in in Astro 4
   integrations: [tailwind(), react()],
   markdown: { syntaxHighlight: 'prism' },
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr'],
-  },
   }
 });
